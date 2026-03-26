87fc5175becb5de3dc3278503e9c8c952f524ab4
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-03-26 18:55:01 +0100

    Add control details layout explorer

---
+++ src/DevTools.Uno/Diagnostics/Views/LayoutExplorerView.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace DevTools.Uno.Diagnostics.Views;
+
+public sealed partial class LayoutExplorerView : UserControl
+{
+    public LayoutExplorerView()
+    {
+        InitializeComponent();
+    }
+}
